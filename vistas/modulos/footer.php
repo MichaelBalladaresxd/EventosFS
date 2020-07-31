99b73719aca0c5f1fad86497c1cc71bb02e2daee
Michael Balladares <51333297+MichaelBalladaresxd@users.noreply.github.com>
2020-07-30 20:14:59 -0500

    footer ok

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 
 <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://www.meichtec.com">Michael Balladares</a>.</strong>
+    <strong>Copyright &copy; 2020 <a href="https://www.meichtec.com" target="_blank">Michael Balladares</a>.</strong>
     Todos los Derechos Reservados.
     <div class="float-right d-none d-sm-inline-block">
       <b>Analista de Sistemas</b>
